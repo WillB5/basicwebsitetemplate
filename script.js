ad40186a5aae5034f3398b27992af3718c7f5a75
William Barrera <65841343+WillB5@users.noreply.github.com>
2022-03-19 22:00:37 +0000

    homepage has the function to reverse a string using JS

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ function reverse(params) {
   let _input = document.StrReverse.StrIn.value;
   let _reversedStr ="";
   for (let index = _input.length-1; index >= 0 ; index--) {
-    _reversedStr += _input[5];
+    _reversedStr += _input[index];
   }
   let _finalStr = document.getElementById("reversed");
-  _finalStr.innerHTML = _reversedStr
+  _finalStr.innerHTML = _reversedStr;
+  document.getElementById("reversedLength").innerHTML = _reversedStr.length;
 }
